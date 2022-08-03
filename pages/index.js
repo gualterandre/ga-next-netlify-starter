ba83976e5986938f0169b5bc5869691c358e2f94
Gualter <gualterandre@gmail.com>
2022-08-03 21:11:35 +0100

    FEATURE title AND SUPER AND GREAT fixed

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title='Welcome to my awesome SUPER FEATURE app!' />
+        <Header title='Welcome to my awesome SUPER GREAT FEATURE app!' />
         <p className='description'>
           Get started by editing <code>pages/index.js</code>
         </p>
